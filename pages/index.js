0f39e1e9bcf9976f8e12bc7cc57c18d2e5ed17dd
riskiriandi <riznevatri11@gmail.com>
2025-07-03 13:27:11 +0700

    index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import MangaGrid from "../components/MangaGrid";
 import { getMangaList, getTopManga } from "../utils/api";
 
-export default async function Home() {
+export async function getStaticProps() {
   const mangaList = await getMangaList();
   const topManga = await getTopManga();
 
+  return {
+    props: {
+      mangaList,
+      topManga,
+    },
+    revalidate: 3600, // auto-refresh tiap 1 jam
+  };
+}
+
+export default function Home({ mangaList, topManga }) {
   return (
     <main className="p-4">
       <h1 className="text-2xl font-bold mb-2">🔥 Populer Hari Ini</h1>
