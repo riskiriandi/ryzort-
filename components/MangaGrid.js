97fd868da94a15a2a6bb668923a6ccb5e367a9d4
riskiriandi <riznevatri11@gmail.com>
2025-07-03 18:05:24 +0700

    MangaGrid.js

--- components/MangaGrid.js
+++ components/MangaGrid.js
@@ -1,6 +1,10 @@
 import MangaCard from "./MangaCard";
 
 export default function MangaGrid({ data }) {
+  if (!data || data.length === 0) {
+    return <p className="text-center text-gray-400">Tidak ada data.</p>;
+  }
+
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
       {data.map((item) => (
@@ -8,4 +12,4 @@ export default function MangaGrid({ data }) {
       ))}
     </div>
   );
-}
+    }
