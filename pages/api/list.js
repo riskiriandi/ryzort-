e925e2c19203e1d0dff7c0513c9b8803209d7a6e
riskiriandi <riznevatri11@gmail.com>
2025-07-03 18:10:23 +0700

    pages/api/list.js

---
+++ pages/api/list.js
@@ -0,0 +1,17 @@
+export default async function handler(req, res) {
+  const headers = {
+    "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Mobile Safari/537.36",
+    "Referer": "https://06.shinigami.asia/",
+    "Sec-Fetch-Site": "same-origin",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Dest": "empty"
+  };
+
+  try {
+    const response = await fetch("https://api.shngm.io/v1/manga/list?type=project&page=1&page_size=24&is_update=true&sort=latest", { headers });
+    const json = await response.json();
+    res.status(200).json(json.data || []);
+  } catch (err) {
+    res.status(500).json({ error: "Gagal ambil data list" });
+  }
+      }
