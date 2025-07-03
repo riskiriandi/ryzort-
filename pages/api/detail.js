fcaeb69893d0eeef5d8d281aaca93e7a8923d037
riskiriandi <riznevatri11@gmail.com>
2025-07-03 18:12:44 +0700

    detail.js

---
+++ pages/api/detail.js
@@ -0,0 +1,19 @@
+export default async function handler(req, res) {
+  const { id } = req.query;
+
+  const headers = {
+    "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Mobile Safari/537.36",
+    "Referer": "https://06.shinigami.asia/",
+    "Sec-Fetch-Site": "same-origin",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Dest": "empty"
+  };
+
+  try {
+    const response = await fetch(`https://api.shngm.io/v1/manga/detail/${id}`, { headers });
+    const json = await response.json();
+    res.status(200).json(json.data || {});
+  } catch (err) {
+    res.status(500).json({ error: "Gagal ambil detail manga" });
+  }
+}
