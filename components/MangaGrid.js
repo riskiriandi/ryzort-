e6a43dc89809d885d974366cc36fd1277ba4cc3f
riskiriandi <riznevatri11@gmail.com>
2025-07-03 13:31:17 +0700

    MangaGrid.js

--- components/MangaGrid.js
+++ components/MangaGrid.js
@@ -1,9 +1,9 @@
 import MangaCard from "./MangaCard";
 
-export default function MangaGrid({ data }: any) {
+export default function MangaGrid({ data }) {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
-      {data.map((item: any) => (
+      {data.map((item) => (
         <MangaCard key={item.id} item={item} />
       ))}
     </div>
